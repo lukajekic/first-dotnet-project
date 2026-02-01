638fa5946fd49d44f1d689b2b31ebbc4545cb8e1
Luka <lukajekic913@gmail.com>
2026-02-01 22:01:19 +0100

    CORS Error fixing

--- Program.cs
+++ Program.cs
@@ -2,10 +2,18 @@ using Notes.Data;
 using Notes.Endpoints;
 using Microsoft.EntityFrameworkCore.Design;
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORS", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
 builder.Services.AddValidation();
 builder.AddNotesDB();
+
 var app = builder.Build();
+app.UseCors("CORS");
 app.AddNotesEndpoints();
 
 app.MigrateDB();
