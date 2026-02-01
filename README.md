f9822074847151071c3d520c6b19d9dce06420f9
Luka <lukajekic913@gmail.com>
2026-02-01 22:18:29 +0100

    README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Notes CRUD
+
+A simple full-stack project for managing notes.
+
+## Tech Stack
+* **Backend:** .NET 8 (ASP.NET Core)
+* **Database:** EF Core
+* **Frontend:** Vanilla JS, HTML, CSS
+
+## Website
+Try it now at:
+https://first-dotnet-frontend.vercel.app/notes
--- obj/Debug/net10.0/Notes.AssemblyInfo.cs
+++ obj/Debug/net10.0/Notes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+befb9f63e6adf4eb9fdf2cc68b02353c1a06e153")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+638fa5946fd49d44f1d689b2b31ebbc4545cb8e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
